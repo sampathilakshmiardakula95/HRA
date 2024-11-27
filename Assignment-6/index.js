3452c643012f5f7b4bd6130f7bf12da3ebf25dba
sampathilakshmiardakula95 <sampathilakshmiardakula@gmail.com>
2024-11-27 14:27:28 +0530

    first commit

--- Assignment-6/index.js
+++ Assignment-6/index.js
@@ -2,10 +2,12 @@ const bgContainerEl = document.getElementById("bgContainer")
 const colorTextEl = document.getElementById("colorText")
 const changeColor = document.getElementById("changeColor")
 const bodyColorEl = document.getElementById('bodyColor')
+const bodyEl = document.getElementById("body")
 const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"]
 
 changeColor.addEventListener('click',()=>{
     let hexcode='#'
+    let bodyhexcode='#'
     for(let i=0;i<6;i++){
         let index = Math.floor(Math.random()*16)
         hexcode=hexcode+arr[index]
@@ -16,9 +18,11 @@ changeColor.addEventListener('click',()=>{
     }
     for(let i=0;i<6;i++){
         let index=Math.floor(Math.random()*16)
-        hexcode=hexcode+arr[index]
-        console.log(hexcode)
-        bodyColorEl.style.backgroundColor=hexcode
+        bodyhexcode=bodyhexcode+arr[index]
+        console.log(bodyhexcode)
+        bodyColorEl.style.backgroundColor=bodyhexcode
+        bodyEl.textContent=bodyhexcode
+        bodyEl.style.color="white"
 
     }
 })
